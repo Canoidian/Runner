38778e57766d01c4804c81bfae8f2beae040bab2
William <eh.canoidian@gmail.com>
2023-04-21 17:20:32 -0400

    update

--- intropygame.py
+++ intropygame.py
@@ -5,6 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 
 while True:
-    # draw all our elements
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+    
+    # draw everything
     # update everything
     pygame.display.update()
