dd1f2a8d756097f3eb17e7b318e76a6842a169f9
William <eh.canoidian@gmail.com>
2023-04-22 20:57:01 -0400

    2:23:56

--- runner.py
+++ runner.py
@@ -1,21 +1,28 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surf = test_font.render(f'Score: {current_time}', False, (64,64,64))
+    score_rect = score_surf.get_rect(center = (400,50))
+    screen.blit(score_surf, score_rect)
+    return current_time
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock() # Creating a clock object to control the game's framerate
-
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
-
-game_active = True
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50) # type: ignore
+game_active = False
+start_time = 0
+score = 0
 
 # new images are on septate surface
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-score_surface = test_font.render('Runner', False, (64,64,64))
-score_rect = score_surface.get_rect(center = (400,50))
+# score_surface = test_font.render('Runner', False, (64,64,64))
+# score_rect = score_surface.get_rect(center = (400,50))
 
 # Make sure to add the "_alpha" when converting the image, to make image background transparent
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
@@ -24,6 +31,17 @@ snail_rect = snail_surface.get_rect(bottomright = (600,300))
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
 player_grav = 0
+
+# Intro screen
+player_stand = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand,0,2)
+player_stand_rect = player_stand.get_rect(center = (400,200))
+
+game_name = test_font.render('Pixel Runner', False, (111,196,169))
+game_name_rect = game_name.get_rect(center = (400,80))
+
+game_message = test_font.render('Press space to run', False, (111,196,169))
+game_message_rect = game_message.get_rect(center = (400,330))
 
 # Checking for Pygame events (such as quitting the game)
 while True:
@@ -44,6 +62,7 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time =  int(pygame.time.get_ticks() / 1000)
 
     # draw all our elements
     # update everything
@@ -52,11 +71,11 @@ while True:
         screen.blit(sky_surface,(0,0))
         # draw the ground
         screen.blit(ground_surface,(0,300))
-        pygame.draw.rect(screen,'#c0eBec',score_rect)
-        pygame.draw.rect(screen,'#c0eBec',score_rect, 10)
         
-        # draw the text
-        screen.blit(score_surface,score_rect)
+        # draw the score
+        score = display_score()
+        # pygame.draw.rect(screen,'#c0eBec',score_rect)
+        # pygame.draw.rect(screen,'#c0eBec',score_rect, 10)
         
         # draw & move the snail
         snail_rect.x -= 4
@@ -69,11 +88,22 @@ while True:
         if player_rect.bottom >= 300: player_rect.bottom = 300
         screen.blit(player_surf,player_rect)
 
-        # collision
-        if snail_rect.colliderect(player_rect):
+        # collision detection
+        if player_rect.colliderect(snail_rect):
             game_active = False
+
     else:
         screen.fill((94,129,162))
+        screen.blit(player_stand,player_stand_rect)
+
+        score_message = test_font.render(f'Your score: {score}', False, (111,196,169))
+        score_message_rect = score_message.get_rect(center = (400,330))
+        screen.blit(game_name,game_name_rect)
+
+        if score == 0: 
+            screen.blit(game_message,game_message_rect)
+        else:
+            screen.blit(score_message,score_message_rect)
         
 
     pygame.display.update()
